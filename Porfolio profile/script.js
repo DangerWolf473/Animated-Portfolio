921a37493e38c91e5ca2f54f676f20eeceb42d5d
Danger Woolf <115442894+DangerWolf473@users.noreply.github.com>
2023-03-10 02:09:33 +0500

    Add files via upload

---
+++ Porfolio profile/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-txt",{
+    strings: ["Frontend Developer", "Video Editor","Gamer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
